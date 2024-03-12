76a5609781c00e67f386bff3bba16baae89da824
Victor Donizete <victor@gw2d.com.br>
2024-03-12 16:42:31 -0300

    starting data generator

---
+++ app.py
@@ -0,0 +1,10 @@
+# Quais valores serão armazenados?
+# email, nome, telefone, estado e cidade
+
+# Quais opções serão liberadas para guardar os dados?
+# .txt, .db(sqlite3) e xlsx
+
+# Organização
+# Vou dividir por modulos 
+
+from values.values import names, emails, telephone, cityByState, state
